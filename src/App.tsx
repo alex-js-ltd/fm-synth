c1d2f1704c1fed6536e057e89093616da7cc4e29
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-06 15:17:25 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useSynth } from 'context/synth-context';
+
 function App() {
+  const { init } = useSynth();
+
+  useEffect(() => {}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => init()}></button>
     </div>
   );
 }
--- src/context/synth-context.tsx
+++ src/context/synth-context.tsx
@@ -3,37 +3,78 @@ import type { ReactNode } from 'react';
 
 type SynthProviderProps = { children: ReactNode };
 
-const SynthContext = createContext<
-  | {
-      analyser: AnalyserNode;
-      modulator: OscillatorNode;
-      carrier: OscillatorNode;
-      modGain: GainNode;
-      masterGain: GainNode;
-    }
-  | undefined
->(undefined);
+type SynthState = {
+  audioCtx: AudioContext;
+  analyser: AnalyserNode;
+  modulator: OscillatorNode;
+  carrier: OscillatorNode;
+  modGain: GainNode;
+  masterGain: GainNode;
+};
+
+type SynthContext = { init: any };
+
+const SynthContext = createContext<SynthContext | undefined>(undefined);
 
 SynthContext.displayName = 'SynthContext';
 
-const audioCtx = new AudioContext();
-const analyser = audioCtx.createAnalyser();
-const modulator = audioCtx.createOscillator();
-const carrier = audioCtx.createOscillator();
-const modGain = audioCtx.createGain();
-const masterGain = audioCtx.createGain();
-
 const SynthProvider = ({ children }: SynthProviderProps) => {
-  const [state, setState] = useState({
-    analyser,
-    modulator,
-    carrier,
-    modGain,
-    masterGain,
-  });
+  const [state, setState] = useState<SynthState | undefined>(undefined);
+
+  function createAudioCtx() {
+    return new Promise<AudioContext>((resolve, reject) => {
+      const audioCtx = new AudioContext();
+      resolve(audioCtx);
+    });
+  }
+
+  function createNodes(audioCtx: AudioContext) {
+    return new Promise<SynthState>((resolve, reject) => {
+      const analyser = audioCtx.createAnalyser();
+      const modulator = audioCtx.createOscillator();
+      const carrier = audioCtx.createOscillator();
+      const modGain = audioCtx.createGain();
+      const masterGain = audioCtx.createGain();
+
+      const synthState = {
+        analyser,
+        modulator,
+        carrier,
+        modGain,
+        masterGain,
+        audioCtx,
+      };
+      resolve(synthState);
+    });
+  }
+
+  function connectNodes(synthState: SynthState) {
+    const { analyser, modulator, carrier, modGain, masterGain, audioCtx } =
+      synthState;
+    modulator.connect(modGain);
+    modGain.connect(carrier.frequency);
+    carrier.connect(masterGain);
+    masterGain.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    modulator.frequency.setValueAtTime(176, audioCtx.currentTime);
+    carrier.frequency.value = 44;
+    modulator.start();
+    carrier.start();
+  }
+
+  async function init(): Promise<SynthState> {
+    const audioCtx = await createAudioCtx();
+    const synthState = await createNodes(audioCtx);
+    connectNodes(synthState);
+
+    console.log(synthState);
+    return synthState;
+  }
+
+  const value = { init };
 
   return (
-    <SynthContext.Provider value={state}>{children} </SynthContext.Provider>
+    <SynthContext.Provider value={value}>{children} </SynthContext.Provider>
   );
 };
 
